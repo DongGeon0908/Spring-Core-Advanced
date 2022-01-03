7cff69ddf0c7064a6c4427eae32563d968898f5a
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-03 16:41:17 +0900

    :bug: 전략 패턴 수정 작업 진행

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -35,34 +38,18 @@ public class ContextV1Test {
         log.info("resultTime={}", resultTime);
     }
 
+
     /**
-     * 템플릿 메서드 패턴 적용
+     * 전략 패턴 사용
      */
     @Test
-    void templateMethodV1() {
-        AbstractTemplate template1 = new SubClassLogic1();
-        template1.execute();
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
 
-        AbstractTemplate template2 = new SubClassLogic2();
-        template2.execute();
-    }
-
-    @Test
-    void templateMethodV2() {
-        AbstractTemplate template1 = new AbstractTemplate() {
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직1 실행");
-            }
-        };
-        template1.execute();
-
-        AbstractTemplate template2 = new AbstractTemplate() {
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직1 실행");
-            }
-        };
-        template2.execute();
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 }
